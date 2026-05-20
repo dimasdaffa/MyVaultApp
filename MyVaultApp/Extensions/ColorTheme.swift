63a359cc9a322d335758596bd370753c00ef885a
dimasdaffa <dimas.de2004@gmail.com>
2026-05-21 00:26:27 +0700

    refactor: remove comment color

--- MyVaultApp/Extensions/ColorTheme.swift
+++ MyVaultApp/Extensions/ColorTheme.swift
@@ -14,22 +14,3 @@ extension Color {
     static let themeRed = Color(red: 255/255, green: 66/255, blue: 69/255) // #FF4245
     static let themeCard = Color(red: 244/255, green: 244/255, blue: 239/255) // #F4F4EF
 }
-
-//extension Color {
-//    static let themePrimary = Color(red: 212/255, green: 112/255, blue: 33/255) // #D47021 (Warm Amber)
-//    static let themeBackground = Color(red: 250/255, green: 250/255, blue: 246/255) // #FAFAF6
-//    static let themeBlack = Color(red: 44/255, green: 41/255, blue: 38/255) // #2C2926
-//    static let themeRed = Color(red: 201/255, green: 75/255, blue: 75/255) // #C94B4B
-//    static let themeCard = Color(red: 242/255, green: 239/255, blue: 233/255) // #F2EFE9
-//    
-//    // BONUS: Added a success color that perfectly complements amber for your "NoBuyResult" screen!
-//    static let themeSuccess = Color(red: 74/255, green: 138/255, blue: 104/255) // #4A8A68 (Sage Green)
-//}
-
-//extension Color {
-//    static let themePrimary = Color(red: 217/255, green: 62/255, blue: 20/255) // #D93E14
-//    static let themeBackground = Color(red: 250/255, green: 250/255, blue: 246/255) // #FAFAF6
-//    static let themeBlack = Color(red: 44/255, green: 41/255, blue: 38/255) // #2C2926 (Warm Espresso)
-//    static let themeRed = Color(red: 201/255, green: 75/255, blue: 75/255) // #C94B4B (Brick Red)
-//    static let themeCard = Color(red: 242/255, green: 239/255, blue: 233/255) // #F2EFE9 (Oatmeal Sand)
-//}
